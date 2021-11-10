db8ee2ecd9ac8ef7ebfec8212b54e7351f988741
giulio30795 <giulioincarbone95@gmail.com>
2021-11-10 15:14:33 +0100

    timer

--- js/script.js
+++ js/script.js
@@ -2,21 +2,28 @@
 // Dopo 30 secondi i numeri in pagina devono essere rimossi e l’utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 // Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.
 // Consigli del giorno:
-// * Pensate prima in italiano.
-// * Dividete in piccoli problemi la consegna.
-// * Individuate gli elementi di cui avete bisogno per realizzare il programma.
+
 const display = document.querySelector('.display')
+// CreazioneArray vuoto
 
 const numeri = [];
 
+// Generazione numeri random e inserimento nell'Array
 for(let i = 0; i < 5; i ++){
      let numero = RandomNum()
     numeri.push(numero)
 };
 
+// Display dell'array
 numeri.forEach((element) => {
     display.innerHTML += `${element} `
 })
+
+// Timer del display con richiesta numeri giocatore
+setTimeout(timergame, 2000)
+
+
+const numerigiocatore = [];
 
 
 
@@ -28,4 +35,12 @@ numeri.forEach((element) => {
 // funzioni
 function RandomNum() {
      return Math.floor( Math.random()* 99 )+1;
+}
+
+function timergame(){
+    display.innerHTML = ''
+    for(let i = 0; i < numeri.length; i++){
+        let risposta = parseInt(prompt('Inserisci un numero'))
+        numerigiocatore.push(risposta)
+    }
 }
